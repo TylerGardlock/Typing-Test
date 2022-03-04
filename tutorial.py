7fe41eee57df719440b13732c69c4b5ad80ec559
TylerGardlock <tgardlock@yahoo.com>
2022-03-04 13:58:32 -0500

    Final commit

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,7 @@
 import curses
 from curses import wrapper
-
+import time
+import random
 
 def start_screen(stdscr):
     stdscr.clear()
@@ -11,7 +12,7 @@ def start_screen(stdscr):
 
 def display_text(stdscr, target_text, current_text, wpm=0):
     stdscr.addstr(target_text)
-    
+    stdscr.addstr(2, 0, f"WPM: {wpm}")
     
     for i, char in enumerate(current_text):
         correct_char = target_text[i]
@@ -21,17 +22,34 @@ def display_text(stdscr, target_text, current_text, wpm=0):
             
         stdscr.addstr(0, i, char, color)
 
+def load_text():
+    with open("text.txt", "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
     
 def wpm_test(stdscr):
-    target_text = "Hello World. Test Text"
+    target_text = load_text()
     current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
     
     while True:
+        time_elasped = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elasped / 60)) / 5)
+        
         stdscr.clear()
-        display_text(stdscr, target_text, current_text)
+        display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()  
         
-        key = stdscr.getkey()
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
+        
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
         
         if ord(key) == 27: # esc key
             break
@@ -39,7 +57,7 @@ def wpm_test(stdscr):
         if key in ("KEY_BACKSPACE", '\b', "\x7f"):
             if len(current_text) > 0:
                 current_text.pop()
-        else:
+        elif len(current_text) < len(target_text):
             current_text.append(key)
 
 def main(stdscr):
@@ -48,6 +66,12 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
-    wpm_test(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(3, 0, "You completed the text! Press any key to continue...")
+        key = stdscr.getkey()
+    
+        if ord(key) == 27:
+            break
 
 wrapper(main)
